8cf15cbf724770d477a765e69e71e849b10a9ca4
Gabriel Alexis F. Gonzales <gfgonzales@donbosco.edu.ph>
2024-07-24 13:28:10 +0800

    ENV2

--- src/components/firebase.tsx
+++ src/components/firebase.tsx
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_APIKEY,
-	authDomain: import.meta.env.VITE_AUTHDOMAIN,
-	projectId: import.meta.env.VITE_PROJECTID,
-	storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-	messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-	appId: import.meta.env.VITE_APPID,
-	measurementId: import.meta.env.VITE_MEASUREMENTID,
+	apiKey: import.meta.env.VITE_VERCEL_APIKEY,
+	authDomain: import.meta.env.VITE_VERCEL_AUTHDOMAIN,
+	projectId: import.meta.env.VITE_VERCEL_PROJECTID,
+	storageBucket: import.meta.env.VITE_VERCEL_STORAGEBUCKET,
+	messagingSenderId: import.meta.env.VITE_VERCEL_MESSAGINGSENDERID,
+	appId: import.meta.env.VITE_VERCEL_APPID,
+	measurementId: import.meta.env.VITE_VERCEL_MEASUREMENTID,
 };
 
 const app = initializeApp(firebaseConfig);
