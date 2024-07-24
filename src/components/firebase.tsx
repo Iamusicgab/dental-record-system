20d4532e32cdd08260f12e928c5ad19e263104a2
Gabriel Alexis F. Gonzales <gfgonzales@donbosco.edu.ph>
2024-07-24 13:18:36 +0800

    Removed StrictMode and ENV

--- src/components/firebase.tsx
+++ src/components/firebase.tsx
@@ -1,16 +1,16 @@
 import { initializeApp } from "@firebase/app";
-import { getFirestore  } from "@firebase/firestore";
-import { getAuth  } from "firebase/auth";
-import { getStorage  } from "firebase/storage";
+import { getFirestore } from "@firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCs4HaxjCCcZ3pWEh0e78hL1q3ikC3RVnI",
-	authDomain: "dental-record-system-c0695.firebaseapp.com",
-	projectId: "dental-record-system-c0695",
-	storageBucket: "dental-record-system-c0695.appspot.com",
-	messagingSenderId: "446706234689",
-	appId: "1:446706234689:web:bf0ba66243dc8568ee5dc0",
-	measurementId: "G-8PHMT0LBN7",
+	apiKey: import.meta.env.VITE_APIKEY,
+	authDomain: import.meta.env.VITE_AUTHDOMAIN,
+	projectId: import.meta.env.VITE_PROJECTID,
+	storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+	messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+	appId: import.meta.env.VITE_APPID,
+	measurementId: import.meta.env.VITE_MEASUREMENTID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,8 +18,7 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
 
-// connectFirestoreEmulator(db, "localhost", 8080);
-// connectAuthEmulator(auth, "http://localhost.:9099");
-// connectStorageEmulator(storage, "localhost", 9199);
+// import firebaseEmulator from "./firebaseEmulator";
+// firebaseEmulator();
 
 export { app, db, auth, storage };
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	// server: {
-	// 	https: {
-	// 		key: "host.key",
-	// 		cert: "host.cert",
-	// 	},
-	// },
+	server: {
+		https: {
+			key: "host.key",
+			cert: "host.cert",
+		},
+	},
+	define: {
+		__APP_ENV__: process.env.VITE_VERCEL_ENV,
+	},
 	plugins: [react()],
 });
